710abff2a0991dc181f8ea0079388c5404968662
tmsgnka <tmsgnka@gmail.com>
2022-09-08 17:21:12 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include >stdio.h> 
+
+/**
+* main - print the string in the put function
+* Description: using the main function
+* this program prints "Programing is like building a multilingual puzzle"
+*/
+int main(void)
+{
+      puts("\"programing is like building a multilingual puzzle");
+      return (0);
+}
