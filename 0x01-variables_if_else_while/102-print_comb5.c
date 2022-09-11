cb74ef3dd22aef66a2bd3c030122f144300eeb94
tmsgnka <tmsgnka@gmail.com>
2022-09-11 13:16:30 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,12 +13,12 @@ for (i = 0; i < 100; i++)
 for (p = 0; p < 100; j++)
 {
 if (p > i)
-{
+{                               
 	putchar((i / 10) + '0');
-	putchar((i % 10) + '0');
+       	putchar((i % 10) + '0');
 	putchar(' ');
 	putchar((p / 10) + '0');
-	putchar((p % 10) + '0');
+       	putchar((p % 10) + '0');
 if (i != 98)
 {
 	putchar(',');
