4a05cdd632e159832f31283047f5f6f3f9034d1a
tmsgnka <tmsgnka@gmail.com>
2022-09-11 13:19:33 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,7 @@
  *
  * Return: returns zero at the end
  */
-int main(void)
+ int main(void)
 {
 int i, p;
 for (i = 0; i < 100; i++)
@@ -13,12 +13,12 @@ for (i = 0; i < 100; i++)
 for (p = 0; p < 100; j++)
 {
 if (p > i)
-{                               
+{
 	putchar((i / 10) + '0');
-       	putchar((i % 10) + '0');
+	putchar((i % 10) + '0');
 	putchar(' ');
 	putchar((p / 10) + '0');
-       	putchar((p % 10) + '0');
+	putchar((p % 10) + '0');
 if (i != 98)
 {
 	putchar(',');
