cb5f9a1e4f7b7a9985b876722d01d6e88128ab4b
tmsgnka <tmsgnka@gmail.com>
2022-09-14 10:38:38 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include "main.h" 
+#include "main.h"
 
-/** 
- * main - print putchar
- * Return: always 0 
- */
+/**
+ *  * main - print putchar
+ *   * Return: always 0
+ *    */
 
-int main(void) 
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -18,4 +18,5 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
+
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-0x02. C - Functions, nested loops
+1 Functions, nested loops
