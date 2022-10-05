4817620fabc6b8efed0fe244a7696214bbc4e410
tmsgnka <tmsgnka@gmail.com>
2022-10-05 07:52:07 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,15 +1,16 @@
+#ifndef MAIN_H
 #include "main.h"
 
 /**
-* create_array - creates an array of chars, and 
+* create_array - creates an array of chars, and
 * initialize it with a specific char.
-*@size: Size of the array 
+*@size: Size of the array
 *@c: Character to insert
 *Return: NULL if size is zero or if it fails,
 *pointer to array if everything is normal.
 */
 
-char *create_array(unsigned in size, char c)
+char *create_array(unsigned int size, char c)
 }
 	char *array;
 	unsigned int i;
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 int _putchar(char);
-char *create_array(unsigned in size, char c);
+char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
@@ -12,4 +12,4 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 
-endif /*MAIN_H */
+#endif /*MAIN_H */
