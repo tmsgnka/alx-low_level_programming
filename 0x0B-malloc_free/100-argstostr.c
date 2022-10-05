ff16d40189101855eb42c2d11a4cf8f1686718f2
tmsgnka <tmsgnka@gmail.com>
2022-10-05 10:59:27 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,12 +1,10 @@
-#include "main"
+#include "main.h"
 
 /**
- * len - returns length of str
- * @str: string counted
- * return: returns the length
- * Return: returns the length
- */
-
+*len - returns length of str
+*@str: string counted
+*Return: returns the length
+*/
 int len(char *str)
 {
 		int len = 0;
@@ -20,22 +18,23 @@ int len(char *str)
 }
 
 /**
- * argstostr -a function that concatenates all the arguments of your program
- * @ac: count of args passed to the function
- * @av:array of arguments
- *
- * Return: pointer to the new string
- */
+* argstostr - a function that concatenates all the arguments of your program
+*@ac: count of args passed to the function
+*@av:array of arguments
+*
+*Return: pointer to the new string
+*/
 
 char *argstostr(int ac, char **av)
 {
 		char *new_string = NULL;
 		int k = 0, i = ac, j, sum = 0, temp = 0;
+
 		if (ac == 0 || av == NULL)
 			return (NULL);
 
 		while (ac--)
-			sum += (len(av[ac]) +1);
+			sum += (len(av[ac]) + 1);
 		new_string = (char *) malloc(sum + 1);
 
 		if (new_string != NULL)
@@ -45,10 +44,10 @@ char *argstostr(int ac, char **av)
 				for (j = 0; av[k][j] != '\0'; j++)
 					new_string[j + temp] = av[k][j];
 				new_string[temp + j] = '\n';
-				temp += (j +1);
+				temp += (j + 1);
 				k++;
 			}
-			new_string[temp] ='\0';
+			new_string[temp] = '\0';
 		}
 		else
 		{
