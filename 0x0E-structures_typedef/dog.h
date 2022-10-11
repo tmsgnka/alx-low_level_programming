0030afa3fcc3a491313f888feb7ff14eb5e02243
tmsgnka <tmsgnka@gmail.com>
2022-10-11 15:53:47 -0700

    puppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,21 @@
 #ifndef DOG_H_FILE
 #define DOG_H_FILE
 
-struct dog
+/**
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ */
+typedef struct dog
 {
-	char name;
-	double age;
-	char owner;
-};
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
-#endif /* DOG_H_FILE */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
