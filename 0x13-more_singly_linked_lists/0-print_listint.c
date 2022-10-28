a41eaae15bcc3364388364cc0829c8e3b6db1ee1
tmsgnka <tmsgnka@gmail.com>
2022-10-28 12:27:51 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,15 +5,10 @@
 
 
 /**
-
  * print_listint -  prints all the elements of a listint_t list.
-
  * @h: list to print
-
  *
-
  * Return: number of elements in the list
-
  */
 
 size_t print_listint(const listint_t *h)
